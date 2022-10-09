00c415af9b95b69c66c26c7085c7a9afd1fc4c7e
NishPatel1 <97016195+NishPatel1@users.noreply.github.com>
2022-10-09 09:58:24 +0530

    Add files via upload

---
+++ Practical11/TripletsExample.java
@@ -0,0 +1,6 @@
+public class TripletsExample {
+    public static void main(String[] args) {
+        Triplet<Integer,String,String> t = new Triplet<Integer,String,String>(Integer.valueOf(10),"Nish","Patel");
+        System.out.println(t);         
+    }
+}
